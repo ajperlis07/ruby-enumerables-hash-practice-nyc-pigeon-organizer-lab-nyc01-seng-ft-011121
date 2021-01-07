6b4df8db25f55ed7ac6addb4dddaa8d1926965a2
Andrew  Perlis <ajperlis07@gmail.com>
2021-01-07 21:12:10 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,23 +1,23 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
-   new_hash = {}
+   pigeon_hash = {}
    data.each do |key, value|
      value.each do |new_value, names|
        names.each do |name|
 
-         if !new_hash[name]
-           new_hash[name] = {}
+         if !pigeon_hash[name]
+           pigeon_hash[name] = {}
          end
 
-         if !new_hash[name][key]
-           new_hash[name][key] = []
+         if !pigeon_hash[name][key]
+           pigeon_hash[name][key] = []
          end
 
-         new_hash[name][key] << new_value.to_s
+         pigeon_hash[name][key] << new_value.to_s
 
        end
      end
    end
-   new_hash
+   pigeon_hash
  end 
 end
